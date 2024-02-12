96692cb2dcd86288b56523c81031e40cd3ee6f69
Maciek Czarnik <czajnikowski@gmail.com>
2024-02-12 17:00:51 +0100

    Add intial boilerplate

---
+++ RefractionAndFun/RefractionAndFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefractionAndFunApp.swift
+//  RefractionAndFun
+//
+//  Created by Maciek Czarnik on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RefractionAndFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
