e622fa970408453bea7ae75303cce78bea9567e9
Maciek Czarnik <czajnikowski@gmail.com>
2024-02-13 20:53:23 +0100

    Add background and foreground, make it use the texture from first pass Essentialize code Move things into more appropriate places

--- RefractionAndFun/RefractionAndFunRenderer.swift
+++ RefractionAndFun/RefractionAndFunRenderer.swift
@@ -10,37 +10,44 @@ import Satin
 import MetalKit
 
 class RefractionAndFunRenderer: BaseRenderer {
-  enum ParamKey: String {
-    case material
-    case position
-  }
-  
-  lazy var icosahedron: Mesh = {
-    let material = BasicDiffuseMaterial(0.7)
-    params[ParamKey.material.rawValue] = material.parameters
-     
-    let mesh = Mesh(
-      geometry: IcoSphereGeometry(radius: 1.0, res: 2),
-      material: material
-    )
-    params[ParamKey.position.rawValue] = mesh.positionParameters
+  lazy var background: Object = {
+    let numberOfSpheres = 10
+    let amountInOneDimensionRange = 0 ..< numberOfSpheres
     
+    return Object(
+      "Background",
+      amountInOneDimensionRange.flatMap { row in
+        amountInOneDimensionRange.map { column in
+          let mesh = Mesh(
+            geometry: IcoSphereGeometry(radius: 0.22, res: 2),
+            material: BasicDiffuseMaterial(0.7)
+          )
+          let centerOffset = Float(numberOfSpheres - 1) / 2
+          mesh.position = .init(
+            x: Float(row) - centerOffset,
+            y: Float(column) - centerOffset,
+            z: 0
+          )
+          
+          return mesh
+        }
+      }
+    )
+  }()
+  private var backgroundTexture: MTLTexture?
+  
+  let foreground: Mesh = {
+    let mesh = Mesh(
+      geometry: IcoSphereGeometry(radius: 2, res: 2),
+      material: nil
+    )
+    mesh.position = [0, 0, -3]
     return mesh
   }()
   
-  var intersectionMesh: Mesh = {
-    let mesh = Mesh(
-      geometry: IcoSphereGeometry(radius: 0.01, res: 2),
-      material: BasicColorMaterial([0.0, 1.0, 0.0, 1.0], .disabled)
-    )
-    
-    mesh.label = "Intersection Mesh"
-    mesh.visible = false
-    return mesh
-  }()
-  lazy var scene = Object("Scene", [icosahedron, intersectionMesh, Object.makeGrid()])
+  lazy var scene = Object("Scene", [background, foreground])
   lazy var context = Context(device, sampleCount, colorPixelFormat, depthPixelFormat, stencilPixelFormat)
-  lazy var camera = PerspectiveCamera(position: .init(repeating: 5.0), near: 0.01, far: 100.0, fov: 30)
+  lazy var camera = PerspectiveCamera(position: [0,0,-10], near: 0.01, far: 100.0, fov: 100)
   lazy var cameraController = PerspectiveCameraController(camera: camera, view: mtkView)
   lazy var renderer = Satin.Renderer(context: context)
   
@@ -60,11 +67,26 @@ class RefractionAndFunRenderer: BaseRenderer {
   }
   
   override func update() {
+    backgroundTexture = createTexture("Background", mtkView.colorPixelFormat)
     cameraController.update()
   }
   
   override func draw(_ view: MTKView, _ commandBuffer: MTLCommandBuffer) {
     guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+    guard let texture = backgroundTexture else { return }
+    
+    foreground.visible = false
+    renderer.draw(
+      renderPassDescriptor: renderPassDescriptor,
+      commandBuffer: commandBuffer,
+      scene: scene,
+      camera: camera,
+      renderTarget: texture
+    )
+    
+    foreground.material = BasicTextureMaterial(texture: texture)
+    
+    foreground.visible = true
     renderer.draw(
       renderPassDescriptor: renderPassDescriptor,
       commandBuffer: commandBuffer,
@@ -78,34 +100,21 @@ class RefractionAndFunRenderer: BaseRenderer {
     renderer.resize(size)
   }
   
-#if os(macOS)
-  override func mouseDown(with event: NSEvent) {
-    intersect(coordinate: normalizePoint(mtkView.convert(event.locationInWindow, from: nil), mtkView.frame.size))
-  }
-  
-#elseif os(iOS)
-  override func touchesBegan(_ touches: Set<UITouch>, with _: UIEvent?) {
-    if let first = touches.first {
-      intersect(coordinate: normalizePoint(first.location(in: mtkView), mtkView.frame.size))
+  func createTexture(_ label: String, _ pixelFormat: MTLPixelFormat) -> MTLTexture? {
+    if mtkView.drawableSize.width > 0, mtkView.drawableSize.height > 0 {
+      let descriptor = MTLTextureDescriptor()
+      descriptor.pixelFormat = pixelFormat
+      descriptor.width = Int(mtkView.drawableSize.width)
+      descriptor.height = Int(mtkView.drawableSize.height)
+      descriptor.sampleCount = 1
+      descriptor.textureType = .type2D
+      descriptor.usage = [.renderTarget, .shaderRead, .shaderWrite]
+      descriptor.storageMode = .private
+      descriptor.resourceOptions = .storageModePrivate
+      guard let texture = device.makeTexture(descriptor: descriptor) else { return nil }
+      texture.label = label
+      return texture
     }
-  }
-#endif
-  
-  func intersect(coordinate: simd_float2) {
-    let results = raycast(camera: camera, coordinate: coordinate, object: scene)
-    if let result = results.first {
-      //            print(result.object.label)
-      //            print(result.position)
-      intersectionMesh.position = result.position
-      intersectionMesh.visible = true
-    }
-  }
-  
-  func normalizePoint(_ point: CGPoint, _ size: CGSize) -> simd_float2 {
-#if os(macOS)
-    return 2.0 * simd_make_float2(Float(point.x / size.width), Float(point.y / size.height)) - 1.0
-#else
-    return 2.0 * simd_make_float2(Float(point.x / size.width), 1.0 - Float(point.y / size.height)) - 1.0
-#endif
+    return nil
   }
 }
